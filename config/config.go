d5e494fae99f9b97113d8ee99ef2d75046c23810
veliashev <auserum@proton.me>
2024-12-21 11:10:45 +0300

    Добавлена json-конфигурация

---
+++ config/config.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"encoding/json"
+	"io"
+	"os"
+)
+
+type Config struct {
+	Port int
+}
+
+func GetConfig() (*Config, error) {
+	path := "config/config.json"
+
+	configFile, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer configFile.Close()
+
+	info, err := io.ReadAll(configFile)
+	if err != nil {
+		return nil, err
+	}
+
+	config := new(Config)
+	err = json.Unmarshal(info, config)
+	if err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
