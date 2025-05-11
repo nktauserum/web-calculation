d217b4af4f92c92fd8f0f4a99b833cffd2fa557a
nktauserum <auserum@proton.me>
2025-05-11 15:34:46 +0300

    #1 исправлена смешная ошибка

--- shared/errors/errors.go
+++ shared/errors/errors.go
@@ -4,7 +4,7 @@ import "errors"
 
 var (
 	ErrMismatchedParentheses = errors.New("несоответствующие скобки")
-	ErrInvalidNumber         = errors.New("недопустимая цифра в выражении")
+	ErrInvalidNumber         = errors.New("недопустимый символ в выражении")
 	ErrInvalidExpression     = errors.New("недопустимое выражение")
 	ErrNotEnoughOperands     = errors.New("недостаточно операндов")
 	ErrDivisionByZero        = errors.New("деление на ноль")
